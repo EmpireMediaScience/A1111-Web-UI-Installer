b8458adac09a615713e58d5dc5b7010f4cd2668b
Leo Mozoloa <leo.drrty@gmail.com>
2022-10-29 18:06:43 +0200

    Update 0.0.4
    Reworked the UI of the installer, so it's a one page thing now :
    • Checkbox to download the model (SD1.4 for now until I find a way to direct DL my 1.5+New VAE Model)
    • It's not placing --xformers or --medvram in the webui-user.bat file anymore, this will be done in the launcher that's planned in a future release
    • Scripts do not show pop ups unless there's an error now
    • New script to install git & python in the background via Winget

--- Advanced Installer/Scripts/dlModel.ps1
+++ Advanced Installer/Scripts/dlModel.ps1
@@ -2,13 +2,11 @@ Add-Type -AssemblyName PresentationFramework
 try {
     $installPath = AI_GetMsiProperty APPDIR 
     $modelsPath = "$installPath\stable-diffusion-webui\models\Stable-diffusion"
-
     $WebClient = New-Object System.Net.WebClient
-    $WebClient.DownloadFile("https://download1980.mediafire.com/0zu8n69plyag/4qazfhjun2n2qs5/model.ckpt", "$modelsPath\model.ckpt")
+    $WebClient.DownloadFile("https://drive.yerf.org/wl/?id=EBfTrmcCCUAGaQBXVIj5lJmEhjoP1tgl&mode=grid&download=1", "$modelsPath\model.ckpt")
 }
 catch {
     [system.windows.messagebox]::Show("Something went wrong with the download", '  Error', 'OK', 'Error')
     return
     exit
 }
-[system.windows.messagebox]::Show("The Model was successfuly downloaded, have fun", '  Success', 'OK', 'Info')
