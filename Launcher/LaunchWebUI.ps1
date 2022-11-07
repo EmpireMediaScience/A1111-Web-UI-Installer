0fe9975852bdf596d7e054557a37c88f41f74e3e
Leo Mozoloa <leo.drrty@gmail.com>
2022-11-07 22:33:51 +0100

    (Kinda) Big Update
    • Updating dialogs in installer
    • New Launcher UI, launched via the "A1111's WebUI (Pin to Taskbar)" shortcut from the install folder
    • The launcher allows you to set some common launch options for the UI: medvram, xformers, ckpt-dir, vae-path
    • It also allows you to auto update the webui & the extensions at launch
    • Warning! the launcher bypasses "webui-user.bat"
    • Settings are saved in /launcher/settings.json

---
+++ Launcher/LaunchWebUI.ps1
@@ -0,0 +1,37 @@
+Param(
+    $settings
+)
+
+Set-Location $PSScriptRoot
+. "$PSScriptRoot\shared.ps1"
+Import-Module "$PSScriptRoot\logger.psm1" -Force -Global -Prefix "logger."
+Import-Module "$PSScriptRoot\Functions.psm1" -Force
+
+git config --global --add safe.directory '*'
+
+# Executing updates
+foreach ($setting in $settings) {
+    if ($setting.arg -ilike "git*" -And $setting.enabled -eq $true) {
+        switch ($setting.arg) {
+            "git-Ext" { 
+                Update-Extensions $true 
+            }
+            "git-UI" { Update-WebUI $true }
+            Default {}
+        }
+    }
+}
+
+# Parsing args from settings
+$arguments = Convert-SettingsToArguments $settings
+
+
+Set-Location $webuiPath
+
+$env:GIT = ""
+$env:PYTHON = "$env:LOCALAPPDATA\Programs\Python\Python310\python.exe"
+$env:VENV_DIR = ""
+$env:COMMANDLINE_ARGS = "--autolaunch " + $arguments
+
+Start-Process "$webuiPath/webui.bat"
+Set-Location $PSScriptRoot
