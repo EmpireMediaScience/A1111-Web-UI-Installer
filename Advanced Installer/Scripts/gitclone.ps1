a9a9c0803f8d996ea43a45838cb13f9550f8c3ee
Leo Mozoloa <leo.drrty@gmail.com>
2022-11-08 01:25:12 +0100

    The installer won't wipe the app directory anymore
    + Some error reporting in case the git clone fails
    + Attempting a fix that deletes the installation files upon uninstall

--- Advanced Installer/Scripts/gitclone.ps1
+++ Advanced Installer/Scripts/gitclone.ps1
@@ -1,17 +1,20 @@
 Add-Type -AssemblyName PresentationFramework
 
 try {
-    $installPath = AI_GetMsiProperty APPDIR 
-    Write-Output "The path is", $installPath
-    if (Test-Path $installPath) {
-        Remove-Item $installPath -Force -Recurse
+    $installPath = AI_GetMsiProperty APPDIR
+    <#     $installPath = "D:\Documents\A1111's Web UI Autoinstaller"     #>
+    Write-Output "The path is $installPath" 
+    $webuiPath = "$installPath\stable-diffusion-webui"
+    if (Test-Path $webuiPath) {
+        Remove-Item $webuiPath -Force -Recurse
     }    
-    New-Item -path $installPath -Force -ItemType Directory -Verbose
     Set-Location $installPath 
     git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
 }
 catch {
-    [system.windows.messagebox]::Show("The WebUI couldn't be cloned from the source URL, something is wrong, contact the support", '  Error', 'OK', 'Error')
+    $err = $_
+    Write-Host $_
+    [system.windows.messagebox]::Show("The WebUI couldn't be cloned from the source URL, something is wrong, contact the support`n`n$err", '  Error', 'OK', 'Error')
     return
     exit
 }
