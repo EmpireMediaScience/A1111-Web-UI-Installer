35e290f4ab0eca2de70ada4325ae281b91d2bcfd
beholdenkey <justthered63@gmail.com>
2023-03-16 17:32:37 -0400

    replace string cont with joinpath

--- Launcher/shared.ps1
+++ Launcher/shared.ps1
@@ -3,28 +3,27 @@ Add-Type -AssemblyName System.Drawing
 Add-Type -AssemblyName PresentationFramework
 Add-Type -AssemblyName Microsoft.VisualBasic
 Set-StrictMode -Version 2
-Import-Module .\logger.psm1 -Force -Global -Prefix "logger."
+Import-Module (Join-Path $PSScriptRoot 'logger.psm1') -Force -Global -Prefix "logger."
 
 # General Variables
 $tempFolder = (Get-Item -Path env:\temp).Value
 $ProgressPreference = 'SilentlyContinue'
 $PATH = [Environment]::GetEnvironmentVariable("PATH", "Machine")
 
-$InstallPath = (get-item $PSScriptRoot ).parent.FullName
-$webuiPath = "$InstallPath\stable-diffusion-webui"
-$modelsPath = "$webuiPath\models\Stable-diffusion"
-$launcherPath = "$InstallPath\Launcher"
-$extPath = "$webuiPath\extensions"
-$settingsPath = ".\settings.json"
-$outputsPath = "$webuiPath\outputs"
-$hashPath = "$webuiPath\.git\refs\heads\master"
-$configFile = "$webuiPath\config.json"
+$InstallPath = (Get-Item $PSScriptRoot).Parent.FullName
+$webuiPath = Join-Path $InstallPath "stable-diffusion-webui"
+$modelsPath = Join-Path $webuiPath "models\Stable-diffusion"
+$launcherPath = Join-Path $InstallPath "Launcher"
+$extPath = Join-Path $webuiPath "extensions"
+$settingsPath = Join-Path $PSScriptRoot "settings.json"
+$outputsPath = Join-Path $webuiPath "outputs"
+$hashPath = Join-Path $webuiPath ".git\refs\heads\master"
+$configFile = Join-Path $webuiPath "config.json"
 
 # Dependencies
-$dependenciesPath = "$InstallPath\Dependencies"
-$gitPath = "$env:ProgramFiles\Git"
+$dependenciesPath = Join-Path $InstallPath "Dependencies"
+$gitPath = Join-Path $env:ProgramFiles "Git"
 $pyPath = ""
-
 
 # Ui general variables
 $backgroundColor = [System.Drawing.ColorTranslator]::FromHtml("#000000")
@@ -33,4 +32,3 @@ $primaryColor = [System.Drawing.ColorTranslator]::FromHtml("#1c0f01")
 $secondaryColor = [System.Drawing.ColorTranslator]::FromHtml("#999999")
 $buttonColor = [System.Drawing.ColorTranslator]::FromHtml("#111111")
 $style = [System.Windows.Forms.FlatStyle]::Flat
-
