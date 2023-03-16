90f4dd21f8d61b73550226afae3320ef6843e69c
beholdenkey <justthered63@gmail.com>
2023-03-16 17:37:22 -0400

    Update dlModel.ps1
        Replaced ! with -not for readability.
        Replaced the string concatenation with the Join-Path cmdlet.
        Changed the casing of system.windows.messagebox to System.Windows.MessageBox for consistency.
        Moved the URL and the local path for the model into separate variables for better readability and maintainability.

--- Advanced Installer/Scripts/dlModel.ps1
+++ Advanced Installer/Scripts/dlModel.ps1
@@ -1,15 +1,18 @@
 Add-Type -AssemblyName PresentationFramework
+
 try {
     $installPath = AI_GetMsiProperty APPDIR
-    $modelsPath = "$installPath\models"
-    if (!(Test-Path $modelsPath)) {
+    $modelsPath = Join-Path $installPath "models"
+    if (-not (Test-Path $modelsPath)) {
         New-Item -ItemType Directory $modelsPath -Force
     }
     $WebClient = New-Object System.Net.WebClient
-    $WebClient.DownloadFile("https://anga.tv/ems/model.ckpt", "$modelsPath\SD15NewVAEpruned.ckpt")
+    $modelUrl = "https://anga.tv/ems/model.ckpt"
+    $modelLocalPath = Join-Path $modelsPath "SD15NewVAEpruned.ckpt"
+    $WebClient.DownloadFile($modelUrl, $modelLocalPath)
 }
 catch {
-    [system.windows.messagebox]::Show("Something went wrong with the download", '  Error', 'OK', 'Error')
+    [System.Windows.MessageBox]::Show("Something went wrong with the download", '  Error', 'OK', 'Error')
     return
     exit
 }
