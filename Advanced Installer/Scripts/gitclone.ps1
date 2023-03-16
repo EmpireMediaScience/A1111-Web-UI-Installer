9c9a2a09e52cb99b9a607ccb9945b9414007f9b9
beholdenkey <justthered63@gmail.com>
2023-03-16 17:35:43 -0400

    Update gitclone.ps1
        Replaced ! with -not for readability.
        Replaced the string concatenation with the Join-Path cmdlet.
        Changed the casing of system.windows.messagebox to System.Windows.MessageBox for consistency.

--- Advanced Installer/Scripts/gitclone.ps1
+++ Advanced Installer/Scripts/gitclone.ps1
@@ -2,12 +2,12 @@ Add-Type -AssemblyName PresentationFramework
 
 try {
     $installPath = AI_GetMsiProperty APPDIR
-    if (!(Test-Path $installPath)) {
+    if (-not (Test-Path $installPath)) {
         Write-Host "Creating $installPath"
         New-Item -ItemType Directory -Path $installPath
     }
     Write-Output "The path is $installPath" 
-    $webuiPath = "$installPath\stable-diffusion-webui"
+    $webuiPath = Join-Path $installPath "stable-diffusion-webui"
     Set-Location $installPath
     if (Test-Path $webuiPath) {        
         Remove-Item $webuiPath -Force -Recurse  
@@ -16,8 +16,7 @@ try {
 catch {
     $err = $_
     Write-Host $_
-    [system.windows.messagebox]::Show("The previous WebUI couldn't be deleted, something is wrong, contact the support`n`n$err", '  Error', 'OK', 'Error')
+    [System.Windows.MessageBox]::Show("The previous WebUI couldn't be deleted, something is wrong, contact the support`n`n$err", '  Error', 'OK', 'Error')
     return
     exit
 }
-
